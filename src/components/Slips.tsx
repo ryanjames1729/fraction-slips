a21c2dcdc954d830cd5d8358b50fe6956dd24e92
Ryan James <ryanjames1729@gmail.com>
2024-04-03 18:41:32 -0400

    working better but still a bug with repeating decimals

--- src/components/Slips.tsx
+++ src/components/Slips.tsx
@@ -1,6 +1,14 @@
 
 import { useState } from 'react'
 
+function closeToZero(decimal: number) {
+    if(decimal < 0.1){
+        return 0
+    } else {
+        return decimal
+    }
+}
+
 function gcd(a: number, b: number)
 {
     if (a == 0)
@@ -15,11 +23,30 @@ function gcd(a: number, b: number)
   
 
 function convertToFraction(decimal: number) {
-    if (Math.round(decimal*100) === 33){
-        return "1/3"
-    } else if (Math.round(decimal*100) === 67) {
-        return "2/3"
+    // check if the decimal is a repeating decimal
+    if(decimal && decimal !== 1){
+        let decimalString = decimal.toString()
+        let decimalArray = decimalString.split(".")
+        let decimalPart = decimalArray[1]
+        let repeating = false
+        for(let i = 0; i < decimalPart.length; i++){
+            if(decimalPart[i] === decimalPart[i+1]){
+                repeating = true
+            }
+        }
+
+        if(repeating) {
+            repeating = false
+            let top = Math.round(decimal*10-decimal)
+            let bot = 9
+            let num = Math.round(top / gcd(top, bot))
+            let deno = Math.round(bot / gcd(top, bot))
+            return num + "/" + deno
+        }
     }
+    
+
+  
 
     // Fetch letegral value of the decimal
     let letVal = Math.floor(decimal);
@@ -81,48 +108,48 @@ export default function Slips() {
             <div className="w-full flex flex-row"><button className={buttonOne ? "w-full border-2 text-center border-blue-500 text-blue-500" : "w-full border-2 text-center"} onClick={()=>{
                 setButtonOne(!buttonOne)
                 if(buttonOne){
-                    setResultOne(resultOne - 1)
+                    setResultOne(closeToZero(resultOne - 1))
                 } else {
-                    setResultOne(resultOne + 1)
+                    setResultOne(closeToZero(resultOne + 1))
                 }
             }}>1</button></div>
             <div className="w-full flex flex-row">
                 <button className={buttonTwo ? "w-1/2 border-2 text-center border-blue-500 text-blue-500" : "w-1/2 border-2 text-center"} onClick={()=>{
                 setButtonTwo(!buttonTwo)
                 if(buttonTwo){
-                    setResultOne(resultOne - 1/2)
+                    setResultOne(closeToZero(resultOne - 1/2))
                 } else {
-                    setResultOne(resultOne + 1/2)
+                    setResultOne(closeToZero(resultOne + 1/2))
                 }}}>1/2</button>
                 <button className={buttonThree ? "w-1/2 border-2 text-center border-blue-500 text-blue-500" : "w-1/2 border-2 text-center"} onClick={()=>{
                 setButtonThree(!buttonThree)
                 if(buttonThree){
-                    setResultOne(resultOne - 1/2)
+                    setResultOne(closeToZero(resultOne - 1/2))
                 } else {
-                    setResultOne(resultOne + 1/2)
+                    setResultOne(closeToZero(resultOne + 1/2))
                 }}}>1/2</button>
                 </div>
             <div className="w-full flex flex-row">
             <button className={buttonFour ? "w-1/3 border-2 text-center border-blue-500 text-blue-500" : "w-1/3 border-2 text-center"} onClick={()=>{
                 setButtonFour(!buttonFour)
                 if(buttonFour){
-                    setResultOne(resultOne - 1/3)
+                    setResultOne(closeToZero(resultOne - 1/3))
                 } else {
-                    setResultOne(resultOne + 1/3)
+                    setResultOne(closeToZero(resultOne + 1/3))
                 }}}>1/3</button>
             <button className={buttonFive ? "w-1/3 border-2 text-center border-blue-500 text-blue-500" : "w-1/3 border-2 text-center"} onClick={()=>{
                 setButtonFive(!buttonFive)
                 if(buttonFive){
-                    setResultOne(resultOne - 1/3)
+                    setResultOne(closeToZero(resultOne - 1/3))
                 } else {
-                    setResultOne(resultOne + 1/3)
+                    setResultOne(closeToZero(resultOne + 1/3))
                 }}}>1/3</button>
             <button className={buttonSix ? "w-1/3 border-2 text-center border-blue-500 text-blue-500" : "w-1/3 border-2 text-center"} onClick={()=>{
                 setButtonSix(!buttonSix)
                 if(buttonSix){
-                    setResultOne(resultOne - 1/3)
+                    setResultOne(closeToZero(resultOne - 1/3))
                 } else {
-                    setResultOne(resultOne + 1/3)
+                    setResultOne(closeToZero(resultOne + 1/3))
                 }}}>1/3</button>
             </div>
 
@@ -130,30 +157,30 @@ export default function Slips() {
             <button className={buttonSeven ? "w-1/4 border-2 text-center border-blue-500 text-blue-500" : "w-1/4 border-2 text-center"} onClick={()=>{
                 setButtonSeven(!buttonSeven)
                 if(buttonSeven){
-                    setResultOne(resultOne - 1/4)
+                    setResultOne(closeToZero(resultOne - 1/4))
                 } else {
-                    setResultOne(resultOne + 1/4)
+                    setResultOne(closeToZero(resultOne + 1/4))
                 }}}>1/4</button>
             <button className={buttonEight ? "w-1/4 border-2 text-center border-blue-500 text-blue-500" : "w-1/4 border-2 text-center"} onClick={()=>{
                 setButtonEight(!buttonEight)
                 if(buttonEight){
-                    setResultOne(resultOne - 1/4)
+                    setResultOne(closeToZero(resultOne - 1/4))
                 } else {
-                    setResultOne(resultOne + 1/4)
+                    setResultOne(closeToZero(resultOne + 1/4))
                 }}}>1/4</button>
             <button className={buttonNine ? "w-1/4 border-2 text-center border-blue-500 text-blue-500" : "w-1/4 border-2 text-center"} onClick={()=>{
                 setButtonNine(!buttonNine)
                 if(buttonNine){
-                    setResultOne(resultOne - 1/4)
+                    setResultOne(closeToZero(resultOne - 1/4))
                 } else {
-                    setResultOne(resultOne + 1/4)
+                    setResultOne(closeToZero(resultOne + 1/4))
                 }}}>1/4</button>
             <button className={buttonTen ? "w-1/4 border-2 text-center border-blue-500 text-blue-500" : "w-1/4 border-2 text-center"} onClick={()=>{
                 setButtonTen(!buttonTen)
                 if(buttonTen){
-                    setResultOne(resultOne - 1/4)
+                    setResultOne(closeToZero(resultOne - 1/4))
                 } else {
-                    setResultOne(resultOne + 1/4)
+                    setResultOne(closeToZero(resultOne + 1/4))
                 }}} >1/4</button>
             </div>
 
@@ -161,42 +188,42 @@ export default function Slips() {
             <button className={buttonEleven ? "w-1/5 border-2 text-center border-blue-500 text-blue-500" : "w-1/5 border-2 text-center"} onClick={()=>{
                 setButtonEleven(!buttonEleven)
                 if(buttonEleven){
-                    setResultOne(resultOne - 1/5)
+                    setResultOne(closeToZero(resultOne - 1/5))
                 } else {
-                    setResultOne(resultOne + 1/5)
+                    setResultOne(closeToZero(resultOne + 1/5))
                 }}}>1/5</button>
             <button className={buttonTwelve ? "w-1/5 border-2 text-center border-blue-500 text-blue-500" : "w-1/5 border-2 text-center"} onClick={()=>{
                 setButtonTwelve(!buttonTwelve)
                 if(buttonTwelve){
-                    setResultOne(resultOne - 1/5)
+                    setResultOne(closeToZero(resultOne - 1/5))
                 } else {
-                    setResultOne(resultOne + 1/5)
+                    setResultOne(closeToZero(resultOne + 1/5))
                 }}
             }>1/5</button>
             <button className={buttonThirteen ? "w-1/5 border-2 text-center border-blue-500 text-blue-500" : "w-1/5 border-2 text-center"} onClick={()=>{
                 setButtonThirteen(!buttonThirteen)
                 if(buttonThirteen){
-                    setResultOne(resultOne - 1/5)
+                    setResultOne(closeToZero(resultOne - 1/5))
                 } else {
-                    setResultOne(resultOne + 1/5)
+                    setResultOne(closeToZero(resultOne + 1/5))
                 }}} >1/5</button>
             <button className={buttonFourteen ? "w-1/5 border-2 text-center border-blue-500 text-blue-500" : "w-1/5 border-2 text-center"} onClick={()=>{
                 setButtonFourteen(!buttonFourteen)
                 if(buttonFourteen){
-                    setResultOne(resultOne - 1/5)
+                    setResultOne(closeToZero(resultOne - 1/5))
                 } else {
-                    setResultOne(resultOne + 1/5)
+                    setResultOne(closeToZero(resultOne + 1/5))
                 }}
             }>1/5</button>
             <button className={buttonFifteen ? "w-1/5 border-2 text-center border-blue-500 text-blue-500" : "w-1/5 border-2 text-center"} onClick={()=>{
                 setButtonFifteen(!buttonFifteen)
                 if(buttonFifteen){
-                    setResultOne(resultOne - 1/5)
+                    setResultOne(closeToZero(resultOne - 1/5))
                 } else {
-                    setResultOne(resultOne + 1/5)
+                    setResultOne(closeToZero(resultOne + 1/5))
                 }}
             }>1/5</button>
-            
+
             </div>
 
         </div>
